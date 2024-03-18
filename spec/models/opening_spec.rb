cb1f46c9cc261b120a8e18feae439d3fd734e795
Rafael Toledo <toledorafael304@gmail.com>
2024-03-18 01:13:07 -0300

    feat: implements simple tests

--- spec/models/opening_spec.rb
+++ spec/models/opening_spec.rb
@@ -4,13 +4,38 @@ require 'rails_helper'
 
 RSpec.describe Opening, type: :model do
   it 'is valid with valid attributes' do
-    opening = Opening.new(role: "teste", company: "teste", location: "teste")
-    opening.
+    opening = Opening.new(role: 'teste', company: 'teste', location: 'teste', remote: true,
+                          link: 'teste.com', salary: 3000)
+    expect(opening).to be_valid
   end
-  it 'is not valid without a role'
-  it 'is not valid without a company'
-  it 'is not valid without a location'
-  it 'is not valid without a remote'
-  it 'is not valid without a link'
-  it 'is not valid without a link'
+  it 'is not valid without a role' do
+    opening = Opening.new(role: nil, company: 'teste', location: 'teste', remote: true,
+                          link: 'teste.com', salary: 3000)
+    expect(opening).not_to be_valid
+  end
+  it 'is not valid without a company' do
+    opening = Opening.new(role: 'teste', company: nil, location: 'teste', remote: true,
+                          link: 'teste.com', salary: 3000)
+    expect(opening).not_to be_valid
+  end
+  it 'is not valid without a location' do
+    opening = Opening.new(role: 'teste', company: 'teste', location: nil, remote: true,
+                          link: 'teste.com', salary: 3000)
+    expect(opening).not_to be_valid
+  end
+  it 'is not valid without a remote' do
+    opening = Opening.new(role: 'teste', company: 'teste', location: 'teste', remote: nil,
+                          link: 'teste.com', salary: 3000)
+    expect(opening).not_to be_valid
+  end
+  it 'is not valid without a link' do
+    opening = Opening.new(role: 'teste', company: 'teste', location: 'teste', remote: true,
+                          link: nil, salary: 3000)
+    expect(opening).not_to be_valid
+  end
+  it 'is not valid without a salary' do
+    opening = Opening.new(role: 'teste', company: 'teste', location: 'teste', remote: true,
+                          link: 'teste', salary: nil)
+    expect(opening).not_to be_valid
+  end
 end
